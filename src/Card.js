a6e6b9d4c5287d82481ae1d6c476f4acfca6ce64
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2021-08-11 17:18:00 -0400

    Added new files to the project

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Card = ()=>{
-    return(
-        <div>
-            <img alt='foto' src=''/>
-            <div>
-                <h1>Jane Doe</h1>
-                <p>jane@example.com</p>
-                </div>
-        </div>
-    )
-}
+const Card = () => {
+  return (
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw5 shadow-5'>
+      <img alt="foto" src="https://robohash.org/test?200x200" />
+      <div>
+        <h1> Jane Doe </h1> <p> jane @example.com </p>{" "}
+      </div>{" "}
+    </div>
+  );
+};
 
 export default Card;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Card />
+    <Card />
     <Card />
   </React.StrictMode>,
   document.getElementById('root')
