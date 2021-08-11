2878e9b22d355a3abd807ef6c0d9eef69fbc844e
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2021-08-11 02:56:46 -0400

    Adding my new React App

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ()=>{
+    return(
+        <div>
+            <img alt='foto' src=''/>
+            <div>
+                <h1>Jane Doe</h1>
+                <p>jane@example.com</p>
+                </div>
+        </div>
+    )
+}
+
+export default Card;
