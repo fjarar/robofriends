5724ec3f71a6325cd778d017fd104efefbde1882
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2021-08-13 18:29:08 -0400

    Was changed h1 tag to h2 tag on Card.js

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({name, email, id}) => {
     <div className='bg-light-green dib br3 pa3 ma2 grow bw5 shadow-5'>
       <img alt="foto" src={`https://robohash.org/${id}?200x200`} />
       <div>
-        <h1>{name}</h1> <p>{email}</p>{" "}
+        <h2>{name}</h2> <p>{email}</p>{" "}
       </div>{" "}
     </div>
   );
