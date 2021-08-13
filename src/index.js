6bfe4fb311922c9fe0d167caa02c96fcaf99c908
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2021-08-12 23:35:28 -0400

    Added SearchBox component and App Component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <CardList robots={robots}/>
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<App />,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
