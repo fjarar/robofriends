977e4022461ce97ad960119644ce5893af727db4
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2021-08-14 16:38:35 -0400

    Added a Scroll component to make better the robo app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -30,13 +31,20 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
     })
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots}/>
-      </div>      
-    );
+    if (this.state.robots.length === 0){
+      return <h1>Loading...</h1>
+    }else{
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots}/>
+          </Scroll>          
+        </div>      
+      );
+    }
+    
   }  
 }
 
